9bce8a62214d66691869f0fb216188a3ae25a5a7
huoxudong <423huo@163.com>
2014-11-10 16:45:57 +0800

    Add ScollViewThumbnail Control

---
+++ ITElite.Projects.WPF.Controls.ScrollViewThumbnail/ReadMe.txt
@@ -0,0 +1,7 @@
+﻿
+WPF ScrollViewer Thumbnail
+http://www.thejoyofcode.com/WPF_ScrollViewer_Thumbnail.aspx
+Controllerizing the ScrollViewer Thumbnail
+http://www.thejoyofcode.com/Controllerizing_the_ScrollViewer_Thumbnail.aspx
+Making the ScrollViewerThumbnail interactive
+http://www.thejoyofcode.com/Making_the_ScrollViewerThumbnail_interactive.aspx
---
+++ ITElite.Projects.WPF.Controls.ScrollViewThumbnail/ScrollViewerThumbnail.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITElite.Projects.WPF.Controls.ScrollViewThumbnail
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ITElite.Projects.WPF.Controls.ScrollViewThumbnail"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ITElite.Projects.WPF.Controls.ScrollViewThumbnail;assembly=ITElite.Projects.WPF.Controls.ScrollViewThumbnail"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class ScrollViewerThumbnail : Control
+    {
+        static ScrollViewerThumbnail()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ScrollViewerThumbnail), new FrameworkPropertyMetadata(typeof(ScrollViewerThumbnail)));
+        }
+
+        public ScrollViewer ScrollViewer
+        {
+            get { return (ScrollViewer)GetValue(ScrollViewerProperty); }
+            set { SetValue(ScrollViewerProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ScrollViewer. This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ScrollViewerProperty =
+        DependencyProperty.Register("ScrollViewer", typeof(ScrollViewer), typeof(ScrollViewerThumbnail), new UIPropertyMetadata(null));
+
+        private const string PART_Highlight = "PART_Highlight";
+
+       
+        public Brush HighlightFill
+        {
+            get { return (Brush)GetValue(HighlightFillProperty); }
+            set { SetValue(HighlightFillProperty, value); }
+        }
+
+        public static readonly DependencyProperty HighlightFillProperty =
+            DependencyProperty.Register("HighlightFill",
+                typeof(Brush),
+                typeof(ScrollViewerThumbnail),
+                new UIPropertyMetadata(new SolidColorBrush(Color.FromArgb(128, 255, 255, 0))));
+
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            var partHighlight = (Thumb)this.Template.FindName(PART_Highlight, this);
+            partHighlight.DragDelta += partHighlight_DragDelta;
+        }
+       private void partHighlight_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            ScrollViewer.ScrollToVerticalOffset(ScrollViewer.VerticalOffset + e.VerticalChange);
+            ScrollViewer.ScrollToHorizontalOffset(ScrollViewer.HorizontalOffset + e.HorizontalChange);
+        }
+    }
+}
